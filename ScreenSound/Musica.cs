2b2ddded3698f17a8a598c4b0f652268bc2d419e
RenanCruz <renancruz199@gmail.com>
2023-12-23 01:04:34 -0300

    Aula 1

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -4,4 +4,20 @@
     public string artista;
     public int duracao;
     public bool disponivel;
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano\n");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Premium\n");
+        }
+    }
 }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,12 +1,14 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "In Constant Sorrow";
 musica1.artista = "SuicideBoys";
-
-Console.WriteLine($"Nome da Musica {musica1.nome} e o artista é {musica1.artista}");
+musica1.duracao = 255;
+musica1.disponivel = false;
+musica1.ExibirFichaTecnica();
 
 
 Musica musica2 = new Musica();
 musica2.nome = "Onde que nós taria";
 musica2.artista = "LEALL";
-
-Console.WriteLine($"Nome da Musica {musica2.nome} e o artista é {musica2.artista}");
+musica2.duracao = 250;
+musica2.disponivel = true;
+musica2.ExibirFichaTecnica();
