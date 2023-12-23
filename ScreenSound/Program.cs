02c9ea5f15d963a3639fc30f3f27ec852402bacc
RenanCruz <renancruz199@gmail.com>
2023-12-23 00:47:43 -0300

    Testando classe

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Musica musica1 = new Musica();
+musica1.nome = "In Constant Sorrow";
+musica1.artista = "SuicideBoys";
+
+Console.WriteLine($"Nome da Musica {musica1.nome} e o artista é {musica1.artista}");
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Onde que nós taria";
+musica2.artista = "LEALL";
+
+Console.WriteLine($"Nome da Musica {musica2.nome} e o artista é {musica2.artista}");
